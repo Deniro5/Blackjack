0afbc802014252aee6cd2e0c89b280cbf7f97e13
USER <user@USERs-MacBook-Pro.local>
2020-03-05 05:33:30 -0500

    removed unused router

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
-import { Router } from 'react-router';
-import { Route } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
 import Home from './Home';
 import '../scss/app.scss';
 
-const history = createHistory();
 
 
 class App extends Component {
   render() {
     return (
-      <Router history={history}>
       <div className="app-container">
-          <Route exact path="/" component={Home} />
+        <Home/>
       </div>
-      </Router>
     );
   }
 }
